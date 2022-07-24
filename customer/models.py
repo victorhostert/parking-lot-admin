8e31547a64f33195405ad727eec7c5aa4699e471
Victor Hostert <victorhostert@gmail.com>
2022-07-24 19:55:48 -0300

    feat: added models and migrations

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class CustomerVehicle(models.Model):
+    KIND_CHOICES = [
+        (1, 'motorcycle'),
+        (2, 'car')
+    ]
+
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    plate = models.CharField(max_length=10)
+    kind = models.CharField(max_length=1, choices=KIND_CHOICES)
+
+    def __str__(self) -> str:
+        return f'Plate: {self.plate} - Customer: {self.customer}'
--- parkmovement/models.py
+++ parkmovement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ParkMovement(models.Model):
+    entry_date = models.DateField()
+    exit_date = models.DateField(null=True, blank=True)
+    value = models.DecimalField(decimal_places=2, max_digits=255)
+    vehicle = models.ForeignKey('customer.CustomerVehicle', on_delete=models.CASCADE, null=True, blank=True)
+    plate = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'Entry: {self.entry_date} Plate: {self.vehicle.plate} Value: {self.value}'
