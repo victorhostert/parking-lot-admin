63aae96f8f5266871a7fae1ce2b37e1f7618b18c
Victor Hostert <victorhostert@gmail.com>
2022-07-24 21:56:24 -0300

    fix: added validation date to park_movement

--- parkmovement/models.py
+++ parkmovement/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class ParkMovement(models.Model):
     entry_date = models.DateField()
     exit_date = models.DateField(null=True, blank=True)
-    value = models.DecimalField(decimal_places=2, max_digits=255)
+    validate_date = models.DateField(null=True, blank=True)
+    value = models.DecimalField(decimal_places=2, max_digits=255, null=True, blank=True)
     vehicle = models.ForeignKey('customer.CustomerVehicle', on_delete=models.CASCADE, null=True, blank=True)
     plate = models.CharField(max_length=50)
 
